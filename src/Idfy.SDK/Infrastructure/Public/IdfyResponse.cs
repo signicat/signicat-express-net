dd0aa539d55f123c088887af1378ab36b82e4305
Christoffer Slettan <christofferslettan@gmail.com>
2018-10-09 14:21:13 +0200

    Add IdfyResponse documentation

--- src/Idfy.SDK/Infrastructure/Public/IdfyResponse.cs
+++ src/Idfy.SDK/Infrastructure/Public/IdfyResponse.cs
@@ -4,10 +4,20 @@ namespace Idfy
 {
     public class IdfyResponse
     {
+        /// <summary>
+        /// The raw JSON returned by Idfy.
+        /// </summary>
         public string ResponseJson { get; set; }
         
+        /// <summary>
+        /// A unique identifier associated with the request. You will be able to use this to search the logs in the Idfy dashboard.
+        /// If you need to contact us about a specific request, providing the request identifier will ensure the fastest possible resolution.
+        /// </summary>
         public string RequestId { get; set; }
         
+        /// <summary>
+        /// The date and time at which the request was made.
+        /// </summary>
         public DateTime RequestDate { get; set; }
     }
 }
