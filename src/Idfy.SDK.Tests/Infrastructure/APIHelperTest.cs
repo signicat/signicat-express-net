014cc7a7263fc6822acccd24f1f418dfcc63f0fa
Christoffer Slettan <christofferslettan@gmail.com>
2018-10-19 12:32:12 +0200

    Update APIHelper.AppendQueryParams test

--- src/Idfy.SDK.Tests/Infrastructure/APIHelperTest.cs
+++ src/Idfy.SDK.Tests/Infrastructure/APIHelperTest.cs
@@ -19,13 +19,14 @@ namespace Idfy.SDK.Tests
                 {"limit", 25},
                 {"fileFormat", FileFormat.StandardPackaging},
                 {"fromDate", new DateTime(2018, 01, 01, 8, 0, 0, DateTimeKind.Utc)},
+                {"toDate", new DateTimeOffset(2018, 01, 01, 8, 0, 0, TimeSpan.Zero)},
                 {"mock", null}
             };
 
             var url = APIHelper.AppendQueryParams("https://api.idfy.io", q);
             
             Assert.IsNotEmpty(url);
-            Assert.AreEqual("https://api.idfy.io?foo=bar&limit=25&fileFormat=standard_packaging&fromDate=2018-01-01T08:00:00Z", url);
+            Assert.AreEqual("https://api.idfy.io?foo=bar&limit=25&fileFormat=standard_packaging&fromDate=2018-01-01T08:00:00.0000000Z&toDate=2018-01-01T08:00:00.0000000+00:00", url);
         }
 
         [Test]
