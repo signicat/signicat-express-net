e02e5c8b63be99cd3e911cccc9265af4d69ada26
vPolland <vegard@idfy.io>
2020-02-06 09:14:00 +0100

    Signature methods cleanup

--- src/Idfy.SDK/Services/Signature/Entities/SignatureMethod.cs
+++ src/Idfy.SDK/Services/Signature/Entities/SignatureMethod.cs
@@ -4,9 +4,6 @@ namespace Idfy.Signature
 {
     public enum SignatureMethod
     {
-        [EnumMember(Value = "no_bankid")]
-        NoBankId = 0,
-    
         [EnumMember(Value = "no_bankid_mobile")]
         NoBankIdMobile = 1,
     
@@ -15,44 +12,20 @@ namespace Idfy.Signature
     
         [EnumMember(Value = "no_buypass")]
         NoBuypass = 3,
-    
-        [EnumMember(Value = "no_commfides")]
-        NoCommfides = 4,
-    
+
         [EnumMember(Value = "se_bankid")]
         SeBankid = 5,
     
         [EnumMember(Value = "dk_nemid")]
         DkNemid = 6,
-    
-        [EnumMember(Value = "fi_tupas")]
-        FiTupas = 7,
-    
-        [EnumMember(Value = "fi_mobiilivarmenne")]
-        FiMobiilivarmenne = 8,
-    
-        [EnumMember(Value = "nl_digid")]
-        NlDigid = 9,
-    
-        [EnumMember(Value = "es_dni")]
-        EsDni = 10,
-    
-        [EnumMember(Value = "ee_esteid")]
-        EeEsteid = 11,
-    
-        [EnumMember(Value = "mobile_connect")]
-        MobileConnect = 12,
-    
+
         [EnumMember(Value = "sms_otp")]
         SmsOtp = 13,
-    
-        [EnumMember(Value = "identification_papers")]
-        IdentificationPapers = 14,
-    
-        [EnumMember(Value = "social")]
-        Social = 15,
-    
+
         [EnumMember(Value = "unknown")]
         Unknown = 16,
+        
+        [EnumMember(Value = "fi_eid")]
+        FiEid = 17
     }
 }
