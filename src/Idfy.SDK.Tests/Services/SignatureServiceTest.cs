20a607535e1f7384fb9aaceca11edc08c3c831d5
vPolland <vegard@idfy.io>
2020-03-18 07:07:58 +0100

    Added new list signature methods endpoint

--- src/Idfy.SDK.Tests/Services/SignatureServiceTest.cs
+++ src/Idfy.SDK.Tests/Services/SignatureServiceTest.cs
@@ -468,5 +468,45 @@ namespace Idfy.SDK.Tests
             Assert.IsNotNull(spinners);
             AssertRequest(HttpMethod.Get, $"/signature/themes/list/spinners");
         }
+        
+        [Test]
+        public void ListSignatureMethods()
+        {
+            var methods = _signatureService.ListSignatureMethods(SignatureMechanism.PkiSignature, 
+                FileType.Pdf, Language.EN, false);
+            
+            Assert.IsNotNull(methods);
+            AssertRequest(HttpMethod.Get, "/signature/signature-methods");
+        }
+        
+        [Test]
+        public async Task ListSignatureMethodsAsync()
+        {
+            var methods = await _signatureService.ListSignatureMethodsAsync(SignatureMechanism.PkiSignature,
+                FileType.Pdf, Language.EN, false);
+            
+            Assert.IsNotNull(methods);
+            AssertRequest(HttpMethod.Get, "/signature/signature-methods");
+        }
+        
+        [Test]
+        public void ListSignatureMethodsForAccount()
+        {
+            var methods = _signatureService.ListSignatureMethodsForAccount(SignatureMechanism.PkiSignature, 
+                FileType.Pdf, Language.EN, false);
+            
+            Assert.IsNotNull(methods);
+            AssertRequest(HttpMethod.Get, "/signature/signature-methods/account");
+        }
+        
+        [Test]
+        public async Task ListSignatureMethodsForAccountAsync()
+        {
+            var methods = await _signatureService.ListSignatureMethodsForAccountAsync(SignatureMechanism.PkiSignature,
+                FileType.Pdf, Language.EN, false);
+            
+            Assert.IsNotNull(methods);
+            AssertRequest(HttpMethod.Get, "/signature/signature-methods/account");
+        }
     }
 }
