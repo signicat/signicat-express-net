4d0acdcc8fb35d343ed835db62c792f93afc62f6
vPolland <vegard@idfy.io>
2020-11-25 08:37:38 +0100

    Add new signature methods

--- src/Idfy.SDK/Services/Signature/Entities/SignatureMethod.cs
+++ src/Idfy.SDK/Services/Signature/Entities/SignatureMethod.cs
@@ -29,6 +29,18 @@ namespace Idfy.Signature
         Unknown = 16,
         
         [EnumMember(Value = "fi_eid")]
-        FiEid = 17
+        FiEid = 17,
+        
+        [EnumMember(Value = "itsme")]
+        Itsme = 18,
+
+        [EnumMember(Value = "idin")]
+        Idin = 19,
+
+        [EnumMember(Value = "digid")]
+        DigiD = 20,
+
+        [EnumMember(Value = "eherkenning")]
+        EHerkenning = 21,
     }
 }
