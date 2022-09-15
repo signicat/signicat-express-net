8787746d63b00ecaa5910e2a13fcc9af281a8994
Christoffer Slettan <c@slettan.no>
2022-09-15 09:31:06 +0200

    Add MitID and new Norwegian BankID to Secure Share

--- src/Signicat.Express.SDK/Services/Share/Entities/Authentication.cs
+++ src/Signicat.Express.SDK/Services/Share/Entities/Authentication.cs
@@ -3,23 +3,23 @@
     public class Authentication
     {
         /// <summary>
-        ///The identity provider type (Norwegian BankID, buypass, Norwegian BankId Mobile etc)
+        /// The identity provider.
         /// </summary>
         public IdentityProvider IdentityProvider { get; set; }
         
         /// <summary>
-        /// The uniqueID from the e-ID, this ID is unique for the user and is the same every time the user logs on. This is not a sensitive ID
-        /// and you could store this to identify the user in you database. Remark: The Swedish BankID do not have an unique ID.
+        /// The uniqueID from the eID. This ID is unique for the user and is the same every time the user logs on. This is not a sensitive ID
+        /// and you could store this to identify the user in you database.
         /// </summary>
         public string IdentityProviderUniqueId { get; set; }
         
         /// <summary>
-        /// Social security number 
+        /// Social security number.
         /// </summary>
         public string Ssn { get; set; }
         
         /// <summary>
-        /// The users date of birth format ddMMyy
+        /// The user's date of birth (ddMMyy).
         /// </summary>
         public string DateOfBirth { get; set; }
 
