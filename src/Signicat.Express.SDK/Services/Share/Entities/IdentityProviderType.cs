8f3e2d6ddd3fe1eb9edb6017539615d435a662f1
Christoffer Slettan <c@slettan.no>
2022-09-15 09:31:06 +0200

    Add MitID and new Norwegian BankID to Secure Share

--- src/Signicat.Express.SDK/Services/Share/Entities/IdentityProviderType.cs
+++ src/Signicat.Express.SDK/Services/Share/Entities/IdentityProviderType.cs
@@ -2,13 +2,15 @@
 {
     public enum IdentityProvider
     {
-        NoBankIdMobile,
-        NoBankIdWeb,
-        SweBankId,
-        SweBankIdMobile,
-        NoBuypass, 
-        DaNemId, 
-        FiTupas,
-        SmsOtp
+        NoBankIdMobile = 0,
+        NoBankIdWeb = 1,
+        SweBankId = 2,
+        SweBankIdMobile = 3,
+        NoBuypass = 4,
+        DaNemId = 5,
+        FiTupas = 6,
+        SmsOtp = 7,
+        NoBankId = 8,
+        MitId = 9,
     }
 }
